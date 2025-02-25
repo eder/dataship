1e8b1daae50041bc2819ba33cae70b1f6ca69e4a
Eder Eduardo <eder.esilva@gmail.com>
2025-02-25 01:07:55 -0300

    model test

--- backend/spec/models/product_spec.rb
+++ backend/spec/models/product_spec.rb
@@ -1,15 +1,56 @@
-# spec/models/product_spec.rb
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it 'is valid with valid attributes' do
-    product = Product.new(name: 'Test', price: 10.0, expiration: Date.tomorrow, exchange_rates: {})
-    expect(product).to be_valid
-  end
+  describe 'scopes' do
+    before do
+      @product1 = create(:product, name: "Apple", price: 100, expiration: Date.today + 10.days)
+      @product2 = create(:product, name: "Banana", price: 200, expiration: Date.today + 20.days)
+      @product3 = create(:product, name: "Carrot", price: 300, expiration: Date.today + 30.days)
+    end
 
-  it 'is invalid without a name' do
-    product = Product.new(price: 10.0, expiration: Date.tomorrow, exchange_rates: {})
-    expect(product).not_to be_valid
+    describe '.by_name' do
+      it 'returns products that match the given name case-insensitively' do
+        expect(Product.by_name("app")).to include(@product1)
+        expect(Product.by_name("app")).not_to include(@product2)
+      end
+
+      it 'returns all products when name is not provided' do
+        expect(Product.by_name(nil)).to eq(Product.all)
+      end
+    end
+
+    describe '.min_price' do
+      it 'returns products with a price greater than or equal to the given value' do
+        expect(Product.min_price(200)).to include(@product2, @product3)
+        expect(Product.min_price(200)).not_to include(@product1)
+      end
+
+      it 'returns all products when price is not provided' do
+        expect(Product.min_price(nil)).to eq(Product.all)
+      end
+    end
+
+    describe '.max_price' do
+      it 'returns products with a price less than or equal to the given value' do
+        expect(Product.max_price(200)).to include(@product1, @product2)
+        expect(Product.max_price(200)).not_to include(@product3)
+      end
+
+      it 'returns all products when price is not provided' do
+        expect(Product.max_price(nil)).to eq(Product.all)
+      end
+    end
+
+    describe '.sorted' do
+      it 'returns products sorted by the given field and order' do
+        sorted_products = Product.sorted('price', 'desc')
+        # Verify that the first product in the sorted list is the one with the highest price
+        expect(sorted_products.first).to eq(Product.order(price: :desc).first)
+      end
+
+      it 'returns all products when an unsupported field is provided' do
+        expect(Product.sorted('unsupported_field', 'asc')).to eq(Product.all)
+      end
+    end
   end
 end
-
